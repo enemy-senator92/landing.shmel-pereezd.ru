dc73d31af252315f734ff7bdaae83c099cf01a26
Максим Донсков <enemy.senator92@gmail.com>
2017-12-13 15:40:31 +0300

    adds first files

---
+++ src/js/vendor.js
@@ -0,0 +1,6 @@
+//= ../lib/modernizr.js
+//= ../lib/jquery-3.2.1.min.js
+//= ../lib/inputmask.js
+//= ../lib/jquery.arcticmodal/jquery.arcticmodal-0.3.min.js
+//= ../lib/fancybox/jquery.fancybox.min.js
+//= ../lib/jquery.slick/slick.min.js
